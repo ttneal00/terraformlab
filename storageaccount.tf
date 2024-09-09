5772e0c72b5b0cbdc535a800388b4a35ce344e9a
Tierre Neal <tneal@hitachisolutions.com>
2024-09-09 08:52:33 -0500

    commit2

--- storageaccount.tf
+++ storageaccount.tf
@@ -21,6 +21,32 @@ resource "azurerm_storage_account" "example" {
   }
 }
 
+resource "azurerm_storage_account" "example2" {
+  name                     = "st2${random_string.storage_account_name.result}"
+  resource_group_name      = "testlab"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled = true
+  
+  tags = {
+    environment = "test"
+  }
+}
+
 output "storage_account_name" {
   value = azurerm_storage_account.example.name
 }
+
+output "storage_account_id" {
+  value = azurerm_storage_account.example.id
+}
+
+output "storage_account_name2" {
+  value = azurerm_storage_account.example2.name
+}
+
+output "storage_account_id2" {
+  value = azurerm_storage_account.example2.id
+}
+
