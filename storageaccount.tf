832460e44de5cc054cd84c344b4446eb5e0f4331
Tierre Neal <tneal@hitachisolutions.com>
2024-09-09 08:27:58 -0500

    updated

--- storageaccount.tf
+++ storageaccount.tf
@@ -9,14 +9,9 @@ resource "random_string" "storage_account_name" {
   special = false
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "testlab"
-  location = "East US"
-}
-
 resource "azurerm_storage_account" "example" {
   name                     = "st${random_string.storage_account_name.result}"
-  resource_group_name      = azurerm_resource_group.example.name
+  resource_group_name      = "testlab"
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
