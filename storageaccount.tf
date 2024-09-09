36932de3a6e226bf23ef6a88c0f4c07c070bbd95
ttneal00 <78926636+ttneal00@users.noreply.github.com>
2024-09-09 08:08:25 -0500

    Create storageaccount.tf

---
+++ storageaccount.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "random_string" "storage_account_name" {
+  length  = 8
+  upper   = false
+  special = false
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-storage-account-test"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "st${random_string.storage_account_name.result}"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  
+  tags = {
+    environment = "test"
+  }
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+}
